8fe18ed44f63bc8f10f5fc736090d4b51f0f9f35
Yosef-ft <yosefetene02@gmail.com>
2024-05-15 23:21:49 +0300

    added changing scene funtionality
    changed DBUtils to Utils because we will have the database functionality in each of the class. The class will be the same as database tables

---
+++ Bank_System/src/page/Utils.java
@@ -0,0 +1,58 @@
+package page;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class Utils extends Application{
+
+    public void ChangeScene(ActionEvent event, String title, String fxmlFile, String email){
+        Parent root = null;
+
+        if(email != null){
+            try{
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+                root = loader.load();
+                //LoggedInController loggedIn = loader.getController();
+
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }else{
+            try{
+                root = FXMLLoader.load(getClass().getResource(fxmlFile));
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(new Scene(root, 600, 400));
+        stage.show();
+    }
+
+    @Override
+    public void start(Stage PrimaryStage){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+            Scene scene = new Scene(root);
+
+            PrimaryStage.setScene(scene);
+            PrimaryStage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
