81f61d2098362fe5410c8f4af1fff664a26cb190
Yosef-ft <yosefetene02@gmail.com>
2024-05-16 14:27:43 +0300

    Added a burger button to display all the functionalities
    Inside the menu bar, did not include the ids and the funtionalities of the button don't forget to add that before moving on

--- Bank_System/src/page/LoggedInController.java
+++ Bank_System/src/page/LoggedInController.java
@@ -2,31 +2,75 @@ package page;
 
 import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
 import java.net.URL;
 
 public class LoggedInController implements Initializable {
 
     @FXML
-    private Button logOut_button;
+    private ImageView exit;
 
     @FXML
-    private Label welcome_label;
+    private ImageView menu;
+
+    @FXML
+    private AnchorPane pane1;
+
+    @FXML
+    private AnchorPane pane2;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        logOut_button.setOnAction(new EventHandler<ActionEvent>() {
 
-            @Override
-            public void handle(ActionEvent event) {
-                Utils.ChangeScene(event, "Log in!", "MainWindow.fxml", null);
-            }
+        pane1.setVisible(false);
+
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), pane1);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), pane2);
+        translateTransition.setByX(-600);
+        translateTransition.play();
+
+        menu.setOnMouseClicked(event ->{
+            pane1.setVisible(true);
+
+            FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(0.5), pane1);
+            fadeTransition1.setFromValue(0);
+            fadeTransition1.setToValue(0.15);
+            fadeTransition1.play();
+    
+            TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(0.5), pane2);
+            translateTransition1.setByX(+600);
+            translateTransition1.play();            
+        });
+
+        pane1.setOnMouseClicked(event ->{
+            FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(0.5), pane1);
+            fadeTransition1.setFromValue(0.15);
+            fadeTransition1.setToValue(0);
+            fadeTransition1.play();
+
+            fadeTransition1.setOnFinished(event1 ->{
+                pane1.setVisible(false);
+            });
+    
+            TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(0.5), pane2);
+            translateTransition1.setByX(-600);
+            translateTransition1.play();  
         });
 
     }
