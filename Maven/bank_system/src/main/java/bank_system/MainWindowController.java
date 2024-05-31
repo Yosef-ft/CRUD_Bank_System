0a1b124d8edd3be15e3208dce04e97e7751307bd
Yosef-ft <yosefetene02@gmail.com>
2024-05-31 03:17:10 +0300

    This is an upgrade of the java project to maven project

---
+++ Maven/bank_system/src/main/java/bank_system/MainWindowController.java
@@ -0,0 +1,48 @@
+package bank_system;
+
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import java.net.URL;
+
+public class MainWindowController implements Initializable {
+
+    @FXML
+    private Button admin_button;
+
+    @FXML
+    private TextField email_lineEdit;
+
+    @FXML
+    private Button login_button;
+
+    @FXML
+    private TextField password_lineEdit;
+
+    @FXML
+    private Button sign_button;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+        login_button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                customer.LogIn(event, email_lineEdit.getText(), password_lineEdit.getText());
+            }
+        });
+
+        sign_button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                Utils.ChangeScene(event, "Create account", "SignIn.fxml", null);
+            }
+        });
+    }
+
+}
